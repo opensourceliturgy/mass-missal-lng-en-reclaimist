6339e9eab7a7c3e7e5138affdae123bc2ecc9fee
Sophia Shapira <Sophia.Shapira@gmail.com>
2016-07-08 21:51:23 -0400

    Added the penitential act.

---
+++ penitential-act.php
@@ -0,0 +1,13 @@
+
+<div class = "leader">Let us now confess our sins.</div>
+<div class = "congr">
+I confess to God, Blessed Mary, all Saints, and to you, that I have sinned exceedingly in thought, word, and deed, of my fault: I pray Holy Mary, all Saints of God, and you, to beseech for me.
+</div>
+<div class = "leader">God Almighty have mercy upon us and forgive us all our sins ; deliver us from every evil ; confirm and strengthen us in goodness and bring us to everlasting life.</div>
+<div class = "congr">Amen.</div>
+
+<?php
+$sm->cite("Adapted from Sarum p. 192 (398 in ebook)");
+$sm->ct_link(array());
+?>
+
